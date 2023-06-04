dbb353c9fdfc0a064d79cfb878b5f89e4d257e91
matheusinho <matheus.luis.developer@gmail.com>
2023-06-04 13:05:38 -0300

    :recycle: refactor: app info constants

--- src/ServiceBusLifeboat.Cli/GroupedConstants/Constants.ApplicationInformations.cs
+++ src/ServiceBusLifeboat.Cli/GroupedConstants/Constants.ApplicationInformations.cs
@@ -4,8 +4,9 @@ public static partial class Constants
 {
     public class ApplicationInformations
     {
-        public const string Description = "A simple AzureServiceBus command line interface.";
-        public const string DefaultConfigurationFolder = ".config";
-        public const string DefaultConnectionStateFile = ".connection_state.json";
+        internal const string Description = "A simple AzureServiceBus command line interface.";
+        internal const string DefaultConfigurationFolder = ".config";
+        internal const string DefaultConnectionStateFile = ".connection_state.json";
+        internal const string DefaultTokenFile = ".token.json";
     }
 }
